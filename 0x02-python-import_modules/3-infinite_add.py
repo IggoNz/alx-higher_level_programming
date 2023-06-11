e2fdedefdbf31ee3e4e70a24a44c0c2f5eda0aa9
Iggo <Iggonzioki@gmail.com>
2023-06-10 22:42:40 -0700

    task 4 and 5

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-if__name__=="__main__":
-    """Print the addition of all arguments"""
+if __name__ = "__main__":
+    """Prints the results of addition of all the arguments."""
     import sys
+
     total = 0
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
-        print("{}.format(total))
+        print("{}".format(total))
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+if __name__ = "__main__":
+    """Print the value of a from the file variable_load_5.py."""
+    from the variable_load_5 import a
+
+    print (a)
