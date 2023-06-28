e7edf775b3d4b8e24c6ecc2bc8eccdf9485efe75
Iggo <Iggonzioki@gmail.com>
2023-06-28 02:13:58 -0700

    task 06

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 6-print_comb3.py
+"""Print allthe possible of two different digits in ascending order.
+the two digits must be different
+"""
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 = 8 and digit2 = 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, diigit2), end=",")
